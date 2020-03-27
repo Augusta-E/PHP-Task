f734afd6299082a0604163bcf8fe31dab0ebc821
netae <ehiheboloaugustar@gmail.com>
2020-03-27 13:14:56 +0100

    My PHP Task 1

--- Processform.php
+++ Processform.php
@@ -1,5 +1,18 @@
 <?php 
+$firstName = $_REQUEST['first_name'];
+$lastName = $_REQUEST['last_name'];
+$email_w = $_REQUEST['email'];
+$department_s = $_REQUEST['department'];
+$message_w = $_REQUEST['message'];
+$gender_s = $_REQUEST['gender'];
+$file_name = $_REQUEST['first_name'];
+$data_ = "First Name:".$firstName . "; ".  "Last Name:".$lastName . "; ". 
+"Email-address:".$email_w . "; ". "Department:".$department_s . "; ". 
+"Gender:".$gender_s . "; " . "Message:" .$message_w;
+$file_extnt   = '.txt';
+$filename = $file_name.$file_extnt;
 
+  file_put_contents( $filename , $data_ );
 print_r($_POST)
 
 ?>
